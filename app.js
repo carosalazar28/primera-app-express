309e0c087038620f31da7242f46a8f333e5c4294
Carolina <cindy.s.r.28@hotmail.com>
2022-02-01 15:43:19 -0500

    while push

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/makers/:name", (req, res) => {
-  function capitalizarPrimeraLetra(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+app.get("/", (req, res) => {
+  const value = 1;
+  while (value < 50) {
+    const typeOfNumber = value % 2 === 0 ? "Par" : "Impar";
+    res.send(`<p>${value} Soy ${typeOfNumber}!</p>`);
+    value += 1;
   }
-
-  const name = req.params.name
-    ? capitalizarPrimeraLetra(req.params.name)
-    : "desconocido";
-  res.send(`<h1>Hola ${name}!</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
