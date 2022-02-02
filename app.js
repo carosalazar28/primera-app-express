4f5c5571efcaf3381b523e5f41fe9c668133d0a8
Carolina <cindy.s.r.28@hotmail.com>
2022-02-02 18:45:23 -0500

    add par and impar

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
+// connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
 
-connection.on("error", function (e) {
-  console.error(e);
-});
+// connection.on("error", function (e) {
+//   console.error(e);
+// });
 
-var articleSchema = mongoose.Schema({
-  title: String,
-  body: String,
-  published: { type: Boolean, default: false },
-});
+// var articleSchema = mongoose.Schema({
+//   title: String,
+//   body: String,
+//   published: { type: Boolean, default: false },
+// });
 
-var Article = mongoose.model("Article", articleSchema);
+// var Article = mongoose.model("Article", articleSchema);
 
-var first = new Article({ title: "Artículo 1", body: "Cuerpo del artículo" });
-first.save(function (err) {
-  if (err) return console.error(err);
-});
+// var first = new Article({ title: "Artículo 1", body: "Cuerpo del artículo" });
+// first.save(function (err) {
+//   if (err) return console.error(err);
+// });
 
 app.get("/", (req, res, next) => {
-  let value = 1;
-  while (value < 50) {
-    let typeOfNumber = value % 2 === 0 ? "Par" : "Impar";
+  let typeOfNumber;
+  for (let i = 1; i <= 50; i += 1) {
+    typeOfNumber = i % 2 === 0 ? "Par" : "Impar";
 
-    res.send(`<p>${value} Soy ${typeOfNumber}!</p>`);
-    value += 1;
+    res.write(`<p>${i} Soy ${typeOfNumber}!</p>`);
   }
+  res.end();
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
